d33b93d32819e9c9cd5b915e2cd7e9e121211cef
andrzejBlazejowski <andrzej.blazejowski@gmail.com>
2023-05-10 20:44:29 +0200

    started to handle login name and messages - serwer

--- src/main/java/org/example/Server.java
+++ src/main/java/org/example/Server.java
@@ -6,18 +6,49 @@ import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.ArrayList;
 
 public class Server implements Runnable {
+    private ArrayList<ConnectionHandler> connections;
+    private ServerSocket socket;
+    private boolean done;
+    public Server(){
+        connections = new ArrayList<>();
+        done = false;
+    }
 
     @Override
     public void run() {
         try {
-            ServerSocket socket = new ServerSocket(8989);
-            Socket client = socket.accept();
-            ConnectionHandler handler = new ConnectionHandler(client);
-            connections.add(handler);
+            while(!done){
+                socket = new ServerSocket(8989);
+                Socket client = socket.accept();
+                ConnectionHandler handler = new ConnectionHandler(client);
+                connections.add(handler);
+
+            }
         } catch (IOException e) {
             System.out.println("could not open socket on serwer side.");
+        }
+    }
+
+    public void broadcast(String msg){
+        for (ConnectionHandler connection : connections){
+            if( connection != null){
+                connection.sendMessage(msg);
+            }
+        }
+    }
+
+    public void shoutdown(){
+        done = true;
+        if (!socket.isClosed()){
+            try{
+                socket.close();
+            }catch (IOException e){
+                System.out.println(e);
+            }
+
         }
     }
 
@@ -35,8 +66,28 @@ public class Server implements Runnable {
                 out = new PrintWriter(client.getOutputStream(), true);
                 in = new BufferedReader(new InputStreamReader(client.getInputStream()));
                 out.println("Please enter a nickname");
-                nickname = in.readLine();
+                login = in.readLine();
                 System.out.println(login + "has joined chat");
+                broadcast(login + "has joined chat");
+                String msg;
+                while((msg = in.readLine()) != null){
+                    if(msg.startsWith("/login ")){
+                        String[] messageSplit = msg.split(" ", 2);
+                        if (messageSplit.length == 2){
+                            broadcast(login + " changed name to " + messageSplit[1]);
+                            System.out.println(login + " changed name to " + messageSplit[1]);
+                            login = messageSplit[1];
+                            System.out.println("changed name success");
+                        }else{
+                            System.out.println("no login entered");
+                        }
+                        // TODO: handle login
+                    }else if(msg.startsWith("/logout ")) {
+                        // TODO: handle logout
+                    }else{
+                        broadcast(login +": "+ msg);
+                    }
+                }
             }catch (IOException e){
                 System.out.println("error");
             }
